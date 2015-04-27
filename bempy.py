4646f033ee29438002018cefb8dabfea7a83cf3f
Clancy Rowley <cwrowley@princeton.edu>
2015-04-27 15:18:57 -0400

    Move NACA code into main module

--- bempy.py
+++ bempy.py
@@ -1,4 +1,3 @@
-import naca
 import numpy as np
 
 class EuclideanTransformation(object):
@@ -102,11 +101,42 @@ class Circle(Body):
 class Airfoil(Body):
     """NACA 4-digit series airfoil
     """
-    def __init__(self, code, num_points, **kwargs):
+    def __init__(self, code, num_points, zero_thick_te=False, uniform=False):
         """Return a NACA 4-digit series airfoil
         """
         super(Airfoil, self).__init__()
-        self._x, self._y = naca.naca4(code, num_points, **kwargs)
+        # extract parameters from 4-digit code
+        code_str = "%04d" % int(code)
+        if len(code_str) != 4:
+            raise ValueError("NACA designation is more than 4 digits")
+        max_camber = 0.01 * int(code_str[0])
+        p = 0.1 * int(code_str[1])  # location of max camber
+        thickness = 0.01 * int(code_str[2:])
+        if uniform:
+            x = np.linspace(0, 1, num_points)
+        else:
+            # closer spacing near leading edge
+            theta = np.linspace(0, 0.5 * np.pi, num_points)
+            x = 1 - np.cos(theta)
+
+        # thickness
+        coefs = [-0.1015, 0.2843, -0.3516, -0.1260, 0, 0.2969]
+        if zero_thick_te:
+            coefs[0] = -0.1036
+        y_thick = 5 * thickness * (np.polyval(coefs[:5], x) +
+                                   coefs[5] * np.sqrt(x))
+
+        # camber
+        front = np.where(x <= p)
+        back = np.where(x > p)
+        y_camber = np.zeros_like(x)
+        if p:
+            y_camber[front] = max_camber * x[front] / p**2 * (2 * p - x[front])
+            y_camber[back] = max_camber * ((1. - x[back])/(1. - p)**2 *
+                                           (1 + x[back] - 2 * p))
+        self._x = np.hstack([x[-1:0:-1], x])
+        self._y = np.hstack([y_camber[-1:0:-1] - y_thick[-1:0:-1],
+                             y_camber + y_thick])
 
     def get_points(self):
         return self._x, self._y
