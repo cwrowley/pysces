Correct sign of vorticity
Induced velocity should be u_theta = Gamma / (2 pi r), so that
circulation about a single point vortex is Gamma.  Previously, the
sign was wrong.

pysces/tests/test_panel.py
@@ -22,7 +22,7 @@ class TestPanel(unittest.TestCase):
         body_panels = BoundVortices(body)
         body_panels.update_strengths()
         gam = body_panels.vortices.strengths
-        self.assertEqual(gam, np.pi)
+        self.assertEqual(gam, -np.pi)
 
     def check_shed_vortex(self, body, wake_fac):
         panels = BoundVortices(body)

pysces/tests/test_vortex.py
@@ -109,21 +109,21 @@ class TestVortex(unittest.TestCase):
         x = (3,0)
         gam = 2 * np.pi
         vel = Vortices().induced_velocity_single(x, v, gam)
-        assert_array_equal(vel, (0,-1))
+        assert_array_equal(vel, (0,1))
 
     def test_induced_velocity_single_array(self):
         v = (0,0)
         x = np.array([(1,0), (2,0), (3,0)])
         gam = 2 * np.pi
         vel = Vortices().induced_velocity_single(x, v, gam)
-        vel_expected = np.array([(0, -1), (0, -1./2), (0, -1./3)])
+        vel_expected = np.array([(0, 1), (0, 1./2), (0, 1./3)])
         assert_array_equal(vel, vel_expected)
 
     def test_induced_velocity_tuple(self):
         v1 = (-1,0)
         v2 = (1,0)
-        g1 = -2 * np.pi
-        g2 = 2 * np.pi
+        g1 = 2 * np.pi
+        g2 = -2 * np.pi
         vort = Vortices([v1, v2], [g1, g2])
         x = (0,0)
         vel = vort.induced_velocity(x)
@@ -133,8 +133,8 @@ class TestVortex(unittest.TestCase):
     def test_induced_velocity_array(self):
         v1 = (-1,0)
         v2 = (1,0)
-        g1 = -2 * np.pi
-        g2 = 2 * np.pi
+        g1 = 2 * np.pi
+        g2 = -2 * np.pi
         vort = Vortices([v1, v2], [g1, g2])
         x = np.array([(0,-1), (0,0), (0,1)])
         vel = vort.induced_velocity(x)
@@ -147,7 +147,7 @@ class TestVortex(unittest.TestCase):
         motion = RigidMotion(np.pi/2, (0,0))
         x = (0,0)
         vel = vort.induced_velocity(x, motion=motion)
-        vel_expected = (-1,0)
+        vel_expected = (1,0)
         assert_array_almost_equal(vel, vel_expected)
 
     def test_regularization(self):
@@ -157,7 +157,7 @@ class TestVortex(unittest.TestCase):
         x0 = np.array((eps,0))
         x = np.array([0.5 * x0, x0, 2 * x0])
         vel = vort.induced_velocity(x)
-        vel_expected = np.array([(0,-0.5/eps), (0,-1./eps), (0,-0.5/eps)])
+        vel_expected = np.array([(0,0.5/eps), (0,1./eps), (0,0.5/eps)])
         assert_array_equal(vel, vel_expected)
 
     def test_advect_1(self):
@@ -170,7 +170,7 @@ class TestVortex(unittest.TestCase):
 
     def test_advect_2(self):
         pos = [(-1,0), (1,0)]
-        gam = 2 * np.pi * np.array([-1, 1])
+        gam = 2 * np.pi * np.array([1, -1])
         vort = Vortices(pos, gam)
         dt = 0.1
         vort.advect(dt)
@@ -181,5 +181,5 @@ class TestVortex(unittest.TestCase):
         vort2 = Vortices((1,0), 2 * np.pi)
         dt = 0.1
         vort1.advect(dt, other=vort2)
-        assert_array_equal(vort1.positions, [(0, dt)])
+        assert_array_equal(vort1.positions, [(0, -dt)])
         assert_array_equal(vort2.positions, [(1, 0)])

pysces/vortex.py
@@ -83,19 +83,19 @@ class Vortices(object):
         -----
         Induced velocity is
 
-        .. math:: u_\theta = -\frac{\Gamma}{2 \pi r}
+        .. math:: u_\theta = \frac{\Gamma}{2 \pi r}
 
         where r is the distance between the point and the vortex.  If this
         distance is less than :class:`core_radius` :math:`r_0`, the velocity is
         regularized as solid-body rotation, with
 
-        .. math:: u_\theta = -\frac{\Gamma r}{2\pi r_0^2}
+        .. math:: u_\theta = \frac{\Gamma r}{2\pi r_0^2}
         """
         r = np.array(x, ndmin=2) - np.array(xvort)
         rsq = np.maximum(np.sum(r * r, 1), self.core_radius**2)
         # alternative regularization (Krasny, Eldredge)
         # rsq = np.sum(r * r, 1) + self.core_radius**2
-        vel = np.transpose(np.array([r[:,1], -r[:,0]]))
+        vel = np.transpose(np.array([-r[:,1], r[:,0]]))
         vel = gam / (2 * np.pi) * vel / rsq[:,np.newaxis]
         return np.squeeze(vel)
 

thin_airfoil.py
@@ -12,8 +12,7 @@ def compute_gam(body):
     panels.update_strengths()
     xvort, gam = panels.vortices.positions, panels.vortices.strengths
     q = body.get_points()
-    ds = np.linalg.norm(np.diff(q, axis=0), axis=1)
-    # import pdb; pdb.set_trace()
+    ds = -np.linalg.norm(np.diff(q, axis=0), axis=1)
     dgam = gam / ds
     s = np.sqrt(xvort[:,0]**2 + xvort[:,1]**2)
     return s, dgam
@@ -30,7 +29,7 @@ half = dgam_airfoil.shape[0] // 2
 dgam_airfoil = dgam_airfoil[:half] + dgam_airfoil[-1:half-1:-1]
 s_airfoil = s_airfoil[:half]
 
-# exact distribution from thin airfoil theory: see Kuethe and Chow p143?
+# exact distribution from thin airfoil theory: see Kuethe and Chow p123
 s = np.linspace(s_plate[-1],1,100)
 dgam_exact = 2 * alpha * np.sqrt((1-s) / s)
 