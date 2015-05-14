3e79281b95169fc654120af01b5a5b81585370f2
Clancy Rowley <cwrowley@princeton.edu>
2015-05-13 22:30:32 -0400

    Print working directory on rtd

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,8 @@ for mod_name in MOCK_MODULES:
 # clean out generated files if running on readthedocs
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
-    shutil.rmtree("generated")
+    print(os.getcwd())
+    # shutil.rmtree("generated")
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
